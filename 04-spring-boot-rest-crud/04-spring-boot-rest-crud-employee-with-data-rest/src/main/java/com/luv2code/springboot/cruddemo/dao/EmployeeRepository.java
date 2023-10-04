66cad2e6f4d9f8c0e6ed6b3a8234ccf1915c96eb
Eduardo Casagrande <eduardocasagrande71@gmail.com>
2023-10-04 15:31:38 -0300

    Refactor - remove controllers and use Data Rest

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,24 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members") // change /employees to /members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // just change the entity type and primary key type and all the CRUD methods are available
-    
+
     // that's it ... all methods are inherited from JpaRepository
+
+
+    // now, the spring-boot-starter-data-rest dependency will automatically
+    // expose the REST endpoints for the entity type specified in the JpaRepository
+
+    // so, we don't need to create a controller for the entity type specified in the JpaRepository
+
+    // ENDPOINTS:
+    // GET /employees
+    // GET /employees/{id}
+    // POST /employees
+    // PUT /employees
+    // DELETE /employees/{id}
 }
